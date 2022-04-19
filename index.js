77ed7ee8b47d21a450997587d51121068477a095
Marco <marcovelardejr@gmail.com>
2022-04-19 17:53:42 -0500

    commit tree f4798d9a439c0b3eedd5dbcd7a1563b772806657

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2; 
+multiply;
+
+
+let random = Math.ceil(Math.random());
+
+let num3 = 14;
+let num4 = 5;
+
+const mod = num3 % num4; 
+
+const max = Math.max(0,1,2,20); 
+
+
+
+
